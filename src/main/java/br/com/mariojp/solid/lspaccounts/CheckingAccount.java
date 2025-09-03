203e69b229a10806ee2854e54f3ba9c773cf34f0
Emilly Marques <emillymarquesx@gmail.com>
2025-09-03 10:15:34 -0300

    Alteração do CheckingAccount

--- src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
@@ -1,9 +1,14 @@
 package br.com.mariojp.solid.lspaccounts;
 
-public class CheckingAccount extends Account {
+public class CheckingAccount extends Account implements Withdrawable {
     @Override
     public void withdraw(double amount) {
-        if(balance < amount) throw new IllegalArgumentException("Saldo insuficiente");
-        super.withdraw(amount);
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Valor inválido");
+        }
+        if (balance < amount) {
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+        this.balance -= amount;
     }
 }
